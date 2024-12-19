c4dca2d484b50d93caa0d4c3f85af08969b7ce77
Nuvindu <nuvidu.18@cse.mrt.ac.lk>
2024-12-19 12:21:28 +0530

    Add necessary util functions for wsdl core

--- wsdl-core/src/main/java/io/ballerina/wsdl/core/Utils.java
+++ wsdl-core/src/main/java/io/ballerina/wsdl/core/Utils.java
@@ -18,35 +18,55 @@
 
 package io.ballerina.wsdl.core;
 
-import io.ballerina.compiler.syntax.tree.AbstractNodeFactory;
-import io.ballerina.compiler.syntax.tree.ImportDeclarationNode;
 import io.ballerina.compiler.syntax.tree.ModuleMemberDeclarationNode;
 import io.ballerina.compiler.syntax.tree.ModulePartNode;
-import io.ballerina.compiler.syntax.tree.NodeList;
 import io.ballerina.compiler.syntax.tree.NodeParser;
 import org.ballerinalang.formatter.core.Formatter;
 import org.ballerinalang.formatter.core.FormatterException;
 import org.ballerinalang.formatter.core.options.ForceFormattingOptions;
 import org.ballerinalang.formatter.core.options.FormattingOptions;
 
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
+import static io.ballerina.wsdl.core.WsdlToBallerina.CHECK;
+import static io.ballerina.wsdl.core.WsdlToBallerina.CLASS;
+import static io.ballerina.wsdl.core.WsdlToBallerina.CLIENT_CONFIG;
+import static io.ballerina.wsdl.core.WsdlToBallerina.CLIENT_ENDPOINT_FIELD;
+import static io.ballerina.wsdl.core.WsdlToBallerina.CLOSE_BRACES;
+import static io.ballerina.wsdl.core.WsdlToBallerina.CLOSE_PARENTHESIS;
+import static io.ballerina.wsdl.core.WsdlToBallerina.COMMA;
+import static io.ballerina.wsdl.core.WsdlToBallerina.CONFIG;
+import static io.ballerina.wsdl.core.WsdlToBallerina.DOT;
+import static io.ballerina.wsdl.core.WsdlToBallerina.EQUALS;
+import static io.ballerina.wsdl.core.WsdlToBallerina.ERROR_OR_NIL;
+import static io.ballerina.wsdl.core.WsdlToBallerina.NEW;
+import static io.ballerina.wsdl.core.WsdlToBallerina.OPEN_BRACES;
+import static io.ballerina.wsdl.core.WsdlToBallerina.OPEN_PARENTHESIS;
+import static io.ballerina.wsdl.core.WsdlToBallerina.RETURN;
+import static io.ballerina.wsdl.core.WsdlToBallerina.RETURNS;
+import static io.ballerina.wsdl.core.WsdlToBallerina.SELF;
 import static io.ballerina.wsdl.core.WsdlToBallerina.SEMICOLON;
+import static io.ballerina.wsdl.core.WsdlToBallerina.SERVICE_URL;
+import static io.ballerina.wsdl.core.WsdlToBallerina.SOAP;
+import static io.ballerina.wsdl.core.WsdlToBallerina.TYPE_INCLUSION;
 import static io.ballerina.wsdl.core.WsdlToBallerina.WHITESPACE;
 import static io.ballerina.xsd.core.visitor.XSDVisitorImpl.EMPTY_STRING;
+import static io.ballerina.xsd.core.visitor.XSDVisitorImpl.QUESTION_MARK;
+import static io.ballerina.xsd.core.visitor.XSDVisitorImpl.RECORD;
 
 public class Utils {
     private static final String XMLDATA_NAME = "@xmldata:Name {value: \"Envelope\"}\n";
-    private static final String XMLDATA_NAMESPACE = "@xmldata:Namespace {prefix: \"soap\", uri: \"http://schemas.xmlsoap.org/soap/envelope/\"}\n";
     private static final String PUBLIC_TYPE = "public type ";
     private static final String RECORD_START = " record {|";
     private static final String RECORD_END = "|};\n";
-    private static final String BODY_FIELD = "    Body;";
+    private static final String BODY_FIELD = "Body?;";
+    public static final String XMLDATA_NAMESPACE = "@xmldata:Namespace {prefix: \"s\", uri: \"%s\"}\n";
+    public static final String SOAP_REQUEST = "SoapRequest";
+    public static final String REQUEST_BODY = "RequestBody";
+    public static final String SOAP_RESPONSE = "SoapResponse";
+    public static final String RESPONSE_BODY = "ResponseBody";
+    public static final String LINE_BREAK = "\n";
 
     public static String formatModuleParts(ModulePartNode modulePartNode) throws FormatterException {
         ForceFormattingOptions forceFormattingOptions = ForceFormattingOptions.builder()
@@ -56,96 +76,62 @@ public class Utils {
         return Formatter.format(modulePartNode.syntaxTree(), formattingOptions).toSourceCode();
     }
 
-    public static void generateTypeDefinitions(String operationName, HashMap<String, ModuleMemberDeclarationNode> nodes,
+    public static void generateTypeDefinitions(String namespace, Map<String, ModuleMemberDeclarationNode> nodes,
                                                String requestType, String responseType) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(XMLDATA_NAME)
-                .append(XMLDATA_NAMESPACE)
-                .append(PUBLIC_TYPE).append("SoapRequest").append(RECORD_START).append("\n")
-                .append("RequestBody").append(WHITESPACE).append(BODY_FIELD).append("\n")
-                .append(RECORD_END);
-
-        nodes.put("SoapRequest", NodeParser.parseModuleMemberDeclaration(sb.toString()));
-
-        sb = new StringBuilder();
-        sb.append(XMLDATA_NAME)
-                .append(XMLDATA_NAMESPACE)
-                .append(PUBLIC_TYPE).append("SoapResponse").append(RECORD_START).append("\n")
-                .append("ResponseBody").append(WHITESPACE).append(BODY_FIELD).append("\n")
-                .append(RECORD_END);
-
-        nodes.put("SoapResponse", NodeParser.parseModuleMemberDeclaration(sb.toString()));
-
-        sb = new StringBuilder();
-        sb.append(XMLDATA_NAMESPACE)
-                .append(PUBLIC_TYPE).append("ResponseBody").append(RECORD_START).append("\n")
-                .append(WHITESPACE + responseType + WHITESPACE + responseType + SEMICOLON).append("\n")
-                .append(RECORD_END);
-
-        nodes.put("ResponseBody", NodeParser.parseModuleMemberDeclaration(sb.toString()));
-
-        sb = new StringBuilder();
-        sb.append(XMLDATA_NAMESPACE)
-                .append(PUBLIC_TYPE).append("RequestBody").append(RECORD_START).append("\n")
-                .append(WHITESPACE + requestType + WHITESPACE + requestType + SEMICOLON).append("\n")
-                .append(RECORD_END);
-
-        nodes.put("RequestBody", NodeParser.parseModuleMemberDeclaration(sb.toString()));
+        generateTypeDefinition(namespace, nodes, SOAP_REQUEST,
+                   REQUEST_BODY + WHITESPACE + BODY_FIELD, true);
+        generateTypeDefinition(EMPTY_STRING, nodes, SOAP_RESPONSE,
+                   RESPONSE_BODY + WHITESPACE + BODY_FIELD, true);
+        generateTypeDefinition(EMPTY_STRING, nodes, RESPONSE_BODY, responseType +
+                               WHITESPACE + responseType + QUESTION_MARK + SEMICOLON, false);
+        generateTypeDefinition(namespace, nodes, REQUEST_BODY, requestType +
+                               WHITESPACE + requestType + QUESTION_MARK + SEMICOLON, false);
     }
 
-    public static NodeList<ImportDeclarationNode> createImportNodes(String... importStatements) {
-        ArrayList<ImportDeclarationNode> nodeList = new ArrayList<>();
-        for (String importStatement : importStatements) {
-            ImportDeclarationNode node = NodeParser.parseImportDeclaration(importStatement);
-            nodeList.add(node);
+    private static void generateTypeDefinition(String namespace, Map<String, ModuleMemberDeclarationNode> nodes,
+                                               String typeName, String bodyContent, boolean includeXmlData) {
+        StringBuilder builder = new StringBuilder();
+        if (includeXmlData) {
+            builder.append(XMLDATA_NAME);
         }
-        return AbstractNodeFactory.createNodeList(nodeList);
+        if (!namespace.equals(EMPTY_STRING)) {
+            builder.append(String.format(XMLDATA_NAMESPACE, namespace));
+        }
+        builder.append(PUBLIC_TYPE).append(typeName).append(WHITESPACE).append(RECORD)
+                .append(OPEN_BRACES).append(LINE_BREAK).append(bodyContent).append(LINE_BREAK)
+                .append(CLOSE_BRACES).append(SEMICOLON);
+        nodes.put(typeName, NodeParser.parseModuleMemberDeclaration(builder.toString()));
     }
 
-    public static Path handleFileOverwrite(Path destinationFile) {
-        if (!Files.exists(destinationFile)) {
-            return destinationFile;
-        }
-        String filePath = destinationFile.toString();
-        int counter = 1;
-        String fileName = new File(filePath).getName();
-        int dotIndex = fileName.lastIndexOf('.');
-        String baseName = dotIndex == -1 ? fileName : fileName.substring(0, dotIndex);
-        String extension = dotIndex == -1 ? EMPTY_STRING : fileName.substring(dotIndex);
-        String parentPath = new File(filePath).getParent() != null ? new File(filePath).getParent() : EMPTY_STRING;
-        while (Files.exists(destinationFile)) {
-            String newFileName = baseName + "." + counter + extension;
-            destinationFile = Path.of(parentPath, newFileName);
-            counter++;
-        }
-        return destinationFile;
-    }
-
-    public static StringBuilder generateClientSkeleton(String soapVersion) {
+    public static StringBuilder generateClientContext(String soapVersion) {
         StringBuilder stringBuilder = new StringBuilder();
         String version = soapVersion.toLowerCase(Locale.ROOT);
-        stringBuilder.append(WsdlToBallerina.PUBLIC).append(WHITESPACE).append(WsdlToBallerina.ISOLATED).append(WHITESPACE).append(WsdlToBallerina.CLIENT)
-                .append(WHITESPACE).append(WsdlToBallerina.CLASS).append(WHITESPACE).append(WsdlToBallerina.CLIENT_NAME).append(WHITESPACE)
-                .append(WsdlToBallerina.OPEN_BRACES);
+        stringBuilder.append(WsdlToBallerina.PUBLIC).append(WHITESPACE).append(WsdlToBallerina.ISOLATED)
+                .append(WHITESPACE).append(WsdlToBallerina.CLIENT).append(WHITESPACE).append(CLASS)
+                .append(WHITESPACE).append(WsdlToBallerina.CLIENT_NAME).append(WHITESPACE)
+                .append(OPEN_BRACES);
+
         stringBuilder.append(WsdlToBallerina.FINAL).append(WHITESPACE).append(version).append(WsdlToBallerina.COLON)
                 .append(WsdlToBallerina.CLIENT_NAME).append(WHITESPACE)
-                .append(WsdlToBallerina.CLIENT_ENDPOINT_FIELD).append(SEMICOLON);
+                .append(CLIENT_ENDPOINT_FIELD).append(SEMICOLON);
 
-        stringBuilder.append(WsdlToBallerina.PUBLIC).append(WHITESPACE).append(WsdlToBallerina.ISOLATED).append(WHITESPACE).append(WsdlToBallerina.FUNCTION)
-                .append(WHITESPACE).append(WsdlToBallerina.INIT).append(WsdlToBallerina.OPEN_PARENTHESIS).append(WsdlToBallerina.STRING).append(WHITESPACE)
-                .append(WsdlToBallerina.SERVICE_URL).append(WsdlToBallerina.COMMA).append(WsdlToBallerina.TYPE_INCLUSION).append(WsdlToBallerina.SOAP).append(WsdlToBallerina.COLON)
-                .append(WsdlToBallerina.CLIENT_CONFIG).append(WHITESPACE).append(WsdlToBallerina.CONFIG).append(WsdlToBallerina.CLOSE_PARENTHESIS)
-                .append(WHITESPACE).append(WsdlToBallerina.RETURNS).append(WHITESPACE).append(WsdlToBallerina.ERROR_OR_NIL).append(WHITESPACE)
-                .append(WsdlToBallerina.OPEN_BRACES).append(WHITESPACE);
+        stringBuilder.append(WsdlToBallerina.PUBLIC).append(WHITESPACE).append(WsdlToBallerina.ISOLATED)
+                .append(WHITESPACE).append(WsdlToBallerina.FUNCTION).append(WHITESPACE).append(WsdlToBallerina.INIT)
+                .append(OPEN_PARENTHESIS).append(WsdlToBallerina.STRING).append(WHITESPACE)
+                .append(SERVICE_URL).append(COMMA).append(TYPE_INCLUSION)
+                .append(SOAP).append(WsdlToBallerina.COLON).append(CLIENT_CONFIG).append(WHITESPACE)
+                .append(CONFIG).append(CLOSE_PARENTHESIS).append(WHITESPACE).append(RETURNS).append(WHITESPACE)
+                .append(ERROR_OR_NIL).append(WHITESPACE).append(OPEN_BRACES).append(WHITESPACE);
 
-        stringBuilder.append(version).append(WsdlToBallerina.COLON).append(WsdlToBallerina.CLIENT_NAME).append(WHITESPACE).append(WsdlToBallerina.SOAP)
-                .append(WHITESPACE).append(WsdlToBallerina.EQUALS).append(WHITESPACE).append(WsdlToBallerina.CHECK).append(WHITESPACE).append(WsdlToBallerina.NEW)
-                .append(WHITESPACE).append(WsdlToBallerina.OPEN_PARENTHESIS).append(WsdlToBallerina.SERVICE_URL).append(WsdlToBallerina.COMMA)
-                .append(WHITESPACE).append(WsdlToBallerina.CONFIG).append(WsdlToBallerina.CLOSE_PARENTHESIS).append(SEMICOLON);
+        stringBuilder.append(version).append(WsdlToBallerina.COLON).append(WsdlToBallerina.CLIENT_NAME)
+                .append(WHITESPACE).append(SOAP).append(WHITESPACE).append(EQUALS).append(WHITESPACE)
+                .append(CHECK).append(WHITESPACE).append(NEW).append(WHITESPACE).append(OPEN_PARENTHESIS)
+                .append(SERVICE_URL).append(COMMA).append(WHITESPACE).append(CONFIG).append(CLOSE_PARENTHESIS)
+                .append(SEMICOLON);
 
-        stringBuilder.append(WsdlToBallerina.SELF).append(WsdlToBallerina.DOT).append(WsdlToBallerina.CLIENT_ENDPOINT_FIELD).append(WHITESPACE).append(WsdlToBallerina.EQUALS)
-                .append(WHITESPACE).append(WsdlToBallerina.SOAP).append(SEMICOLON).append(WsdlToBallerina.RETURN)
-                .append(SEMICOLON).append(WsdlToBallerina.CLOSE_BRACES);
+        stringBuilder.append(SELF).append(DOT).append(CLIENT_ENDPOINT_FIELD).append(WHITESPACE).append(EQUALS)
+                .append(WHITESPACE).append(SOAP).append(SEMICOLON).append(RETURN)
+                .append(SEMICOLON).append(CLOSE_BRACES);
         return stringBuilder;
     }
 }
